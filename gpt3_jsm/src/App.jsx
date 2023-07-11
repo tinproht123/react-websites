09b32c4f18648766c4cbdc8563bd59ddda07b2b4
tinproht123 <tinproht123@gmail.com>
2023-07-11 15:36:54 +0700

    first commit

---
+++ gpt3_jsm/src/App.jsx
@@ -0,0 +1,13 @@
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Article />
+      <Brand />
+    </div>
+  );
+};
+
+export default App;
---
+++ gpt3_jsm/src/components/article/index.jsx
@@ -0,0 +1,7 @@
+import "./article.css";
+
+const Article = () => {
+  return <div>Article</div>;
+};
+
+export default Article;
