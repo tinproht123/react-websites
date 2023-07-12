1cd3ae8a851581f567db73237c6d998f688b8c41
tinproht123 <tinproht123@gmail.com>
2023-07-12 16:13:19 +0700

    modified

--- gpt3_jsm/src/components/navbar/index.jsx
+++ gpt3_jsm/src/components/navbar/index.jsx
@@ -1,7 +1,34 @@
 import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="gpt3__navbar-links-container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
